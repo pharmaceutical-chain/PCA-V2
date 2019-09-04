d8b05025b68ca2d2d61cec464c5203f86a3da815
Lee Khai <leekhai18@gmail.com>
2019-09-05 01:21:54 +0700

    Init

---
+++ projects/angular-ngrx-material-starter/src/app/features/examples/examples/examples.component.ts
@@ -0,0 +1,42 @@
+import { Store, select } from '@ngrx/store';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import {
+  routeAnimations,
+  selectIsAuthenticated
+} from '../../../core/core.module';
+
+import { State } from '../examples.state';
+
+@Component({
+  selector: 'pca-examples',
+  templateUrl: './examples.component.html',
+  styleUrls: ['./examples.component.scss'],
+  animations: [routeAnimations],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ExamplesComponent implements OnInit {
+  isAuthenticated$: Observable<boolean>;
+
+  examples = [
+    { link: 'todos', label: 'pca.examples.menu.todos' },
+    { link: 'stock-market', label: 'pca.examples.menu.stocks' },
+    { link: 'theming', label: 'pca.examples.menu.theming' },
+    { link: 'crud', label: 'pca.examples.menu.crud' },
+    {
+      link: 'simple-state-management',
+      label: 'pca.examples.menu.simple-state-management'
+    },
+    { link: 'form', label: 'pca.examples.menu.form' },
+    { link: 'notifications', label: 'pca.examples.menu.notifications' },
+    { link: 'elements', label: 'pca.examples.menu.elements' },
+    { link: 'authenticated', label: 'pca.examples.menu.auth', auth: true }
+  ];
+
+  constructor(private store: Store<State>) {}
+
+  ngOnInit(): void {
+    this.isAuthenticated$ = this.store.pipe(select(selectIsAuthenticated));
+  }
+}
