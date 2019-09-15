677efe9ae354022366914c920c2cba834b2c74eb
Lee Khai <leekhai18@gmail.com>
2019-09-15 23:50:20 +0700

    Generate tenant module and enter component

---
+++ projects/pharma-chain-application/src/app/features/tenant/enter-tenant/enter-tenant.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'pca-enter-tenant',
+  templateUrl: './enter-tenant.component.html',
+  styleUrls: ['./enter-tenant.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class EnterTenantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
