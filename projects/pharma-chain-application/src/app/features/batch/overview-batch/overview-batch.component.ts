57146bfe35a511dc780fc5b6807d85d222da6371
Lee Khai <leekhai18@gmail.com>
2019-10-24 00:47:40 +0700

    Init

--- projects/pharma-chain-application/src/app/features/batch/overview-batch/overview-batch.component.ts
+++ projects/pharma-chain-application/src/app/features/batch/overview-batch/overview-batch.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ROUTE_ANIMATIONS_ELEMENTS } from '../../../core/core.module';
+import { FormBuilder } from '@angular/forms';
+import { PageEvent } from '@angular/material';
 
 @Component({
   selector: 'pca-overview-batch',
@@ -8,9 +11,39 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class OverviewBatchComponent implements OnInit {
 
-  constructor() { }
+  routeAnimationsElements = ROUTE_ANIMATIONS_ELEMENTS;
+
+  currentPageNumberControl = this.fb.control(1, { updateOn: 'blur' });
+  currentPageIndex = 0;
+  length = 100;
+  pageSize = 10;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+
+  sortOptions = ['Business name', 'Created date'];
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
 
+  // Trigger blur page number input
+  onblur() {
+    const maxPage = Math.ceil(this.length / this.pageSize);
+
+    if (this.currentPageNumberControl.value < 1) {
+      this.currentPageNumberControl.setValue(1);
+    }
+    if (this.currentPageNumberControl.value > maxPage) {
+      this.currentPageNumberControl.setValue(maxPage);
+    }
+
+    this.currentPageIndex = this.currentPageNumberControl.value - 1;
+  }
+
+  // Trigger event of paginator without page number input
+  pageEvent(event: PageEvent) {
+    console.log(event);
+    this.currentPageNumberControl.setValue(event.pageIndex + 1);
+    this.pageSize = event.pageSize;
+  }
 }
